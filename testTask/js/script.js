3ba7627088f4b4a77c0cd6ed969d1b1ddeded318
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2023-09-15 16:01:04 +0300

    Delete testTask directory

--- testTask/js/script.js
+++
@@ -1,8 +0,0 @@
-const closeBtn = document.querySelector('.js-btn__close');
-const bannerEl = document.querySelector('.js-banner__wrapper');
-
-closeBtn.addEventListener('click', onCloseBtnClick);
-
-function onCloseBtnClick (evt) {
-  bannerEl.classList.add('is-close');
-}
